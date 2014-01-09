ad3ddba29c8bc78551e9c0faceac3f793f34f63d
igorpecovnik <igor.pecovnik@gmail.com>
2014-01-09 19:10:17 +0100

    Switched default Kernel source repo

--- build.sh
+++ build.sh
@@ -53,7 +53,8 @@ if [ -d "$DEST/linux-sunxi" ]
 then
 	cd $DEST/linux-sunxi; git pull -f; cd $SRC
 else
-	git clone https://github.com/cubieboard/linux-sunxi/ $DEST/linux-sunxi # Kernel 3.4.61+
+	# git clone https://github.com/cubieboard/linux-sunxi/ $DEST/linux-sunxi # Kernel 3.4.61+
+	git clone https://github.com/patrickhwood/linux-sunxi -b pat-3.4.75-ct $DEST/linux-sunxi # Patwood's kernel 3.4.75+
 fi
 
 # Applying Patch for 2gb memory
