005f2cccb0d50fd0aaed491ef2f20feb2a6a0755
igorpecovnik <igor.pecovnik@gmail.com>
2014-01-09 19:01:14 +0100

    setting new min/max speed for cpu

--- build.sh
+++ build.sh
@@ -193,8 +193,8 @@ chroot $DEST/output/sdcard /bin/bash -c "apt-get -qq -y install openssh-server c
 chroot $DEST/output/sdcard /bin/bash -c "apt-get -qq -y upgrade"
 
 # configure MIN / MAX Speed for cpufrequtils
-sed -e 's/MIN_SPEED="0"/MIN_SPEED="30000"/g' -i $DEST/output/sdcard/etc/init.d/cpufrequtils
-sed -e 's/MAX_SPEED="0"/MAX_SPEED="1000000"/g' -i $DEST/output/sdcard/etc/init.d/cpufrequtils
+sed -e 's/MIN_SPEED="0"/MIN_SPEED="480000"/g' -i $DEST/output/sdcard/etc/init.d/cpufrequtils
+sed -e 's/MAX_SPEED="0"/MAX_SPEED="1010000"/g' -i $DEST/output/sdcard/etc/init.d/cpufrequtils
 
 # set password to 1234
 chroot $DEST/output/sdcard /bin/bash -c "(echo 1234;echo 1234;) | passwd root" 
