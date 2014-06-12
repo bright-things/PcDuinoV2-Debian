da8b3bc20d902e6c03372e389ebbdfe77f36e044
Igor Pecovnik <igor.pecovnik@gmail.com>
2014-06-12 21:41:46 +0200

    needed some fixes, otherwise stuck at boot

--- build.sh
+++ build.sh
@@ -263,7 +263,7 @@ chroot $DEST/output/sdcard /bin/bash -c "chmod +x /usr/local/bin/brcm_patchram_p
 cp $SRC/scripts/brcm40183 $DEST/output/sdcard/etc/default
 cp $SRC/scripts/brcm40183-patch $DEST/output/sdcard/etc/init.d
 chroot $DEST/output/sdcard /bin/bash -c "chmod +x /etc/init.d/brcm40183-patch"
-chroot $DEST/output/sdcard /bin/bash -c "update-rc.d brcm40183-patch defaults" 
+#chroot $DEST/output/sdcard /bin/bash -c "update-rc.d brcm40183-patch defaults" 
 
 # install custom bashrc
 #cp $SRC/scripts/bashrc $DEST/output/sdcard/root/.bashrc
